cf6a813ac2a06d90ab8a98c3e50cfc2ea57b701d
Bishops Hull Hub <bishopshullhub@gmail.com>
2026-04-13 13:26:07 +0000

    Can you replace the Her image with this one: https://bhhub.co.uk/wp-cont

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'bhhub.co.uk',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
