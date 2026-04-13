a563eaa7d54ed5f7a8ab22bf3e1e6451c0c1504a
Bishops Hull Hub <bishopshullhub@gmail.com>
2026-04-13 14:35:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-567413641-bce0e",
+  "appId": "1:62094398243:web:4d59a2fce76a123f8af296",
+  "apiKey": "AIzaSyAoweeTQMIbFOJoEGxe5BbZYq-MI4GHYEU",
+  "authDomain": "studio-567413641-bce0e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "62094398243"
+};
