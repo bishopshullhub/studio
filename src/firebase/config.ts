cabe1ba2860b993c61c88a3a1287c0af9a94dedc
Bishops Hull Hub <bishopshullhub@gmail.com>
2026-04-14 10:25:08 +0000

    at the moment the code is leaking the secrets: src/firebase/config.ts ex

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-567413641-bce0e",
-  "appId": "1:62094398243:web:4d59a2fce76a123f8af296",
-  "apiKey": "AIzaSyAoweeTQMIbFOJoEGxe5BbZYq-MI4GHYEU",
-  "authDomain": "studio-567413641-bce0e.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "62094398243"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "",
 };
